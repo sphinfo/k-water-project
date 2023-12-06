411357a5d09d5c37045a6c2c8b29333842f70a0f
BORA JEONG <brjeong@sphinfo.co.kr>
2023-12-06 16:50:40 +0900

    [UI] 맵 위젯 스타일 변경

--- src/components/map/right/ZoomMapWidget.js
+++ src/components/map/right/ZoomMapWidget.js
@@ -23,16 +23,16 @@ const ZoomMapWidget = () => {
     return (
         <ul className="map-widget-vertical-block map-basic-style">
           <button className="mapRightBt" onClick={home}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="none"><path stroke="#3D3D3D" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 4.527V2m0 0h-2.774M15 2l-4 4M3 11.473V14m0 0h2.774M3 14l4-4M12.473 14H15m0 0v-2.774m0 2.773-4-4M5.527 2H3m0 0v2.774M3 2l4 4"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="none"><path stroke="rgba(255,255,255,0.8)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 4.527V2m0 0h-2.774M15 2l-4 4M3 11.473V14m0 0h2.774M3 14l4-4M12.473 14H15m0 0v-2.774m0 2.773-4-4M5.527 2H3m0 0v2.774M3 2l4 4"/></svg>
           </button>
             <button className="mapRightBt" onClick={zoomIn}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path d="M8 8V1.5M8 8V14.5M8 8H14.5M8 8H1.5" stroke="#3D3D3D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M8 8V1.5M8 8V14.5M8 8H14.5M8 8H1.5" stroke="rgba(255,255,255,0.8)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </button>
             <button className="mapRightBt" onClick={zoomOut}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="2" viewBox="0 0 16 2" fill="none">
-                    <path d="M1.5 1H14.5" stroke="#3D3D3D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M1.5 1H14.5" stroke="rgba(255,255,255,0.8)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </button>
             {/* <button className="mapRightBt" onClick={add}>
