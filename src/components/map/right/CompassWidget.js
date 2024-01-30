88544b76ed68fdbed336c1b541098dd29d7f6140
Bora Jeong <brjeong@sphinfo.co.kr>
2024-01-30 15:02:21 +0900

    [UI] 맵툴바 아이콘 추가, 툴팁 추가

--- src/components/map/right/CompassWidget.js
+++ src/components/map/right/CompassWidget.js
@@ -1,5 +1,6 @@
 import MapEvents from '@common/eventBus/MapEvents';
 import EventBus from '@common/eventBus/eventBus';
+import Tooltip from '@mui/material/Tooltip';
 import React, { useEffect, useState } from 'react';
 
 const CompassWidget = () => {
@@ -12,12 +13,23 @@ const CompassWidget = () => {
         })
     },[])
 
-
     return (
         <>
+          <Tooltip placement="left"
+                   TransitionProps={{ timeout: 200 }}
+                   arrow
+                   title={
+                    <React.Fragment>
+                        <div className="tooltip-info-map">
+                            <h6>나침반/방위</h6>
+                            <p>Ctrl + 지도 드래그</p>
+                        </div>
+                    </React.Fragment>
+                  }>
             <div className="map-compass" style={{transform: `rotate(${heading}deg)`}}>
-                <div className="map-compass-pin"></div>
+              <div className="map-compass-pin"></div>
             </div>
+          </Tooltip>
         </>
     )
 }
